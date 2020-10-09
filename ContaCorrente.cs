a2426e091480f40ede5c6153e7d2f2649356e87d
Silas Stoffel <silasstofel@gmail.com>
2020-10-09 20:47:02 -0300

    Primeira classe

---
+++ ContaCorrente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class ContaCorrente
+    {
+        public string titular;
+        public string agencia;
+        public string numero;
+        public double saldo;
+
+        public ContaCorrente()
+        {
+            this.saldo = 0;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (!this.PossuiSaldo(valor))
+            {
+                return false;
+            }
+            this.saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        private bool PossuiSaldo(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.saldo = 100.00;
+            Console.WriteLine("Saldo: " + conta.saldo);
+            double valorSaque = 50.00;
+            Console.WriteLine("Sacando: R$ " + valorSaque);
+            conta.Sacar(valorSaque);
+            Console.WriteLine("Saldo Atualizado: R$ " + conta.saldo);
+           
+            double valorDeposito = 250.00;
+            Console.WriteLine("Depositando: R$ " + valorDeposito);
+            conta.Depositar(valorDeposito);
+            Console.WriteLine("Saldo Atualizado: R$ " + conta.saldo);
+
+            valorSaque = 450.00;
+            Console.WriteLine("Tentando sacar: R$ " + valorSaque);
+            bool sacou = conta.Sacar(valorSaque);
+            if (!sacou)
+                Console.WriteLine("Saldo insuficiente");
+
+            Console.WriteLine("Saldo Atualizado: R$ " + conta.saldo);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Aperte qualquer tecla para finalizar ...");
+            Console.ReadLine();
+
+        }
+    }
+}
